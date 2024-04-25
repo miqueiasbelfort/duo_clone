991e0fc32814333b9587aec1ce992e15a7c52b52
Miqueias Kawa <kawasousa1554@gmail.com>
2024-04-25 20:16:30 -0300

    seed script

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -18,14 +18,14 @@ async function LearnPage() {
         <div className='flex flex-row-reverse gap-[48px] px-6'>
             <StickerWrapper>
                 <UserProgress
-                    activeCourse={{ title: "Spanish", imageSrc: '/es.svg' }}
-                    hearts={5}
-                    points={100}
+                    activeCourse={userProgress.activeCourse}
+                    hearts={userProgress.hearts}
+                    points={userProgress.points}
                     hasActiveSubscription={false}
                 />
             </StickerWrapper>
             <FeedWrapper>
-                <Header title="Spanish" />
+                <Header title={userProgress.activeCourse.title} />
             </FeedWrapper>
         </div>
     );
